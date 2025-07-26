73ecc5a0e608e17b98a47edb24f38296fa382aaf
HubSabia <hubsabia@gmail.com>
2025-07-26 15:51:22 -0300

    Mudança no componente de Campaigns Table e outras
    Melhorei o design da pagina de Campaigns View, ao invés de usar cards como planejado optei por usar uma tabela por possuir uma poluição visual menor, resolvi a questão do nome da pagina não aparecer no header e removi a preview do chatbot para colocar a pagina de gerenciamento de bots

---
+++ router/index.js
@@ -0,0 +1,53 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import('@/views/LoginView.vue'),
+    meta: { title: 'Login' }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('@/views/DashboardView.vue'),
+    meta: { title: 'Dashboard', requiresAuth: true }
+  },
+  {
+    path: '/campanhas',
+    name: 'Campanhas',
+    component: () => import('@/views/CampaignsView.vue'),
+    meta: { title: 'Gestão de Campanhas', requiresAuth: true }
+  },
+  {
+    path: '/chatbots',
+    name: 'Chatbots',
+    component: () => import('@/views/ChatbotsView.vue'),
+    meta: { title: 'Gestão de Chatbots', requiresAuth: true }
+  },
+  {
+    path: '/usuarios',
+    name: 'Usuarios',
+    component: () => import('@/views/UsersView.vue'),
+    meta: { title: 'Gestão de Usuários', requiresAuth: true }
+  },
+  {
+    path: '/sobre-bot',
+    name: 'SobreBot',
+    component: () => import('@/views/SobreBotView.vue'),
+    meta: { title: 'Sobre o Bot', requiresAuth: true }
+  },
+  {
+    path: '/chatbot',
+    name: 'ChatBot',
+    component: () => import('@/views/ChatBotView.vue'),
+    meta: { title: 'Chat Bot', requiresAuth: true }
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
